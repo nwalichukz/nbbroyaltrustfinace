e270e8b978afb03cf48a93378444e6412133055a
nwalichukz <chukznwali@gmail.com>
2026-08-26 19:35:15 +0100

    ok move

--- nbbroyaltrustfinance/app/Services/ZeptoMailService.php
+++ nbbroyaltrustfinance/app/Services/ZeptoMailService.php
@@ -4,17 +4,25 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 
+
 class ZeptoMailService
-{
+{ 
+
+public static $token="Zoho-enczapikey wSsVR61+8xDyB659z2GlcbttnQkHAFP/ERt/3FT0vX+tHKzDpcc+wkDPDAGkGPkfRGc9EGEWrbksmxhShmcPhtgrzFAHWyiF9mqRe1U4J3x17qnvhDzDXmlblheKLosBxQ5immJjE84r+g==";
+public static $url="https://api.zeptomail.eu/v1.1/email";
+public static $from="hello@kuritr.com"
+public static $from_name="NbbTrustKapital";
+
+
     public static function send(string $toEmail, string $toName, string $subject, string $htmlBody): array
     {
         $response = Http::withHeaders([
-            'Authorization' => env('ZEPTOMAIL_TOKEN'),
+            'Authorization' => self::$token,
             'Content-Type' => 'application/json',
-        ])->post(env('ZEPTOMAIL_URL'), [
+        ])->post(self::$url, [
             'from' => [
-                'address' => env('ZEPTOMAIL_FROM'),
-                'name' => env('ZEPTOMAIL_FROM_NAME'),
+                'address' => self::$from,
+                'name' => self::$from_name,
             ],
             'to' => [[
                 'email_address' => ['address' => $toEmail, 'name' => $toName],
