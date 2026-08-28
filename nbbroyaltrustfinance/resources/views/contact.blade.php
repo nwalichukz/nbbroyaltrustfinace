98473a151a043a6c32a4ada809ccbc5fd57cad55
nwalichukz <chukznwali@gmail.com>
2026-08-28 02:29:39 +0100

    ya we go

--- nbbroyaltrustfinance/resources/views/contact.blade.php
+++ nbbroyaltrustfinance/resources/views/contact.blade.php
@@ -22,7 +22,7 @@
                         <span class="contact-info-card__icon"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8"><path d="M22 16.9v3a2 2 0 01-2.2 2 19.8 19.8 0 01-8.6-3 19.5 19.5 0 01-6-6 19.8 19.8 0 01-3-8.7A2 2 0 014.1 2h3a2 2 0 012 1.7c.1 1 .3 2 .7 3a2 2 0 01-.4 2.1L8 10.3a16 16 0 006 6l1.5-1.4a2 2 0 012.1-.4c1 .4 2 .6 3 .7a2 2 0 011.7 2z"/></svg></span>
                         <div>
                             <h3>Reach us</h3>
-                            <p><a href="tel:+442012345678"> </a><br>Mon&ndash;Fri, 8am&ndash;7pm GMT</p>
+                            <p><a href="tel:+442012345678"> +44</a><br>Mon&ndash;Fri, 8am&ndash;7pm GMT</p>
                         </div>
                     </div>
 
